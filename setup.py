aa8bb8c19996064a12896b7bf00f61adf24e8a1d
Joshua Meier <joshim5@openai.com>
2018-07-22 20:41:04 -0700

    turn into pip module

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='retro-baselines',
+    version='0.0.0',
+    url='https://github.com/openai/retro-baselines',
+    py_modules=['retro_baselines'],
+)
